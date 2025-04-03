785d07de7084d5dd63eb6a6903a63c084851408a
Aaron Campos <aaroncm611@gmail.com>
2025-04-03 11:03:53 +0200

    catalogo hecho

--- src/app.py
+++ src/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("catalogo.html")
 
+@app.route("/contacto", methods=["GET"])
+def index():
+    return render_template("contacto.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
