089a20279b8b3c4c9451abdb85f1714deb343bc4
Toni Díaz <tonidiazfdez@gmail.com>
2025-04-10 11:15:08 +0200

    cambios menores

--- src/app.py
+++ src/app.py
@@ -23,8 +23,7 @@ def contacto():
             # Aquí podrías agregar la lógica para enviar el formulario o guardarlo en una base de datos
 
             flash("Formulario enviado con éxito", "success")
-            return redirect(url_for("catalogo"))
-    return render_template("contacto.html")
+    return render_template("contacto.html", form=contactForm)
 
 @app.route("/somos", methods=["GET"])
 def somos():
