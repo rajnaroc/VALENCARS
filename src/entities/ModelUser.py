790614b07f387775527df0386de7592e26d92b56
Aaron Campos <aaroncm611@gmail.com>
2025-05-05 11:59:41 +0200

    bd

--- src/entities/ModelUser.py
+++ src/entities/ModelUser.py
@@ -23,35 +23,20 @@ class ModelUser:
             print(e)
 
     @classmethod
-    def register(cls, db, fullname, email, password):
-        try:
-            User(fullname,email,password)
-            cursor = db.connection.cursor()
-            hashed_password = User.hash_password(password)  
-            cursor.execute("INSERT INTO users (id, fullname, email, password) VALUES (NULL, %s, %s, %s)", (fullname, email, hashed_password))
-            
-            db.connection.commit()
-            cursor.close()
-
-            return True
-        except Exception as e:
-            print(e)
-
-    @classmethod
     def login(cls,db,email,password):
         try:
             cur = db.connection.cursor()
-            cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+            cur.execute("SELECT * FROM admin WHERE email = %s", (email,))
             data = cur.fetchone()
 
             if data:
                 id = data[0]
-                fullname = data[1]
-                password = data[2]
-                email = data[3]
+                nombre = data[1]
+                email = data[2]
+                password = data[3]
                 valor = User.check_password(password,user.password)
                 if valor:
-                    user = User(id,fullname,None,email)
+                    user = User(id,nombre,None,email)
 
                     return user
                 return print("error password")
